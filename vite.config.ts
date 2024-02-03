ec40ab8c14c6c9928edc89cc3e9185b76c75e8e4
Klyuchnikov <96Dimian@inbox.ru>
2024-02-03 11:06:33 +0300

    feature/remove-initial-components configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,44 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
-import react from '@vitejs/plugin-react'
+import { defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { htmlPlugin } from './environment/vite/plugins/_htmlPlugin';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      // Ployfill the Electron and Node.js built-in modules for Renderer process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: {},
-    }),
-  ],
-})
+export default defineConfig(({mode}) => {
+    return {
+        plugins: [
+            react(),
+            htmlPlugin(mode)
+        ],
+        test: {
+          globals: true,
+          environment: 'jsdom',
+        },
+        resolve: {
+            alias: {
+                'src': path.resolve(__dirname, 'src'),
+                'styles': path.resolve(__dirname, 'src/styles'),
+                'assets': path.resolve(__dirname, 'src/assets'),
+                'mocks': path.resolve(__dirname, 'src/mocks'),
+                'models': path.resolve(__dirname, 'src/models'),
+                'types': path.resolve(__dirname, 'src/types'),
+                'enums': path.resolve(__dirname, 'src/enums'),
+                'constants': path.resolve(__dirname, 'src/constants'),
+                'services': path.resolve(__dirname, 'src/services'),
+                'tools': path.resolve(__dirname, 'src/tools'),
+                'routers': path.resolve(__dirname, 'src/routers'),
+                'routes': path.resolve(__dirname, 'src/routes'),
+                'store': path.resolve(__dirname, 'src/store'),
+                'hocs': path.resolve(__dirname, 'src/hocs'),
+                'hooks': path.resolve(__dirname, 'src/hooks'),
+                'layouts': path.resolve(__dirname, 'src/layouts'),
+                'pages': path.resolve(__dirname, 'src/pages'),
+                'portals': path.resolve(__dirname, 'src/portals'),
+                'components': path.resolve(__dirname, 'src/components')
+            }
+        },
+        base: '/',
+        build: {
+            assetsDir: 'assets'
+        }
+    };
+});
